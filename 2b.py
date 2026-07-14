9fe1784736a97282148ab7321176af382096dac1
supazuto <mongkolbb48@gmail.com>
2026-07-14 16:48:52 +0700

    [Fixed] 2b.py for bad condition

--- 2b.py
+++ 2b.py
@@ -2,23 +2,27 @@ name = input("name : ")
 age = int(input("age :  "))
 if age >= 20:
     print("PASSED")
+    height = float(input("height(cm) : "))
+    if height >= 150:
+        print("PASSED")
+        power = int(input("power : "))
+        if power >= 5:
+            print("PASSED")
+            collect = float(input("collect : "))
+            if collect >= 5:
+                print("PASSED")
+                
+                print("Congratulation, Welcome to ours team!!")
+                print(f'Mr/Mrs.{name} || Age {age} years old || height {height} cm || power {power} || money {collect}')
+            else:
+                print("Too Poor")
+        else:
+            print("Too Weak")
+    else:
+        print("Too SHORT")
 else:
     print("Too young")
-height = float(input("height(cm) : "))
-if height >= 150:
-    print("PASSED")
-else:
-    print("Too SHORT")
-power = int(input("power : "))
-if power >= 5:
-    print("PASSED")
-else:
-    print("Too Weak")
-collect = float(input("collect : "))
-if collect >= 5:
-    print("PASSED")
-else:
-    print("Too Poor")
 
-print("Congratulation, Welcome to ours team!!")
-print(f'Mr/Mrs.{name} || Age {age} years old || height {height} cm || power {power} || money {collect}')
+
+
+
